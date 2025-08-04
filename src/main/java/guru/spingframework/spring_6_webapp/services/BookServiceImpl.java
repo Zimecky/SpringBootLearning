2e7e7d88449c582367e72b354554bf232264c8a1
Zimecki <szymon.z@vp.pl>
2025-08-04 16:02:48 +0200

    Add BookService interface and BookServiceImpl

---
+++ src/main/java/guru/spingframework/spring_6_webapp/services/BookServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.spingframework.spring_6_webapp.services;
+
+import guru.spingframework.spring_6_webapp.domain.Book;
+import guru.spingframework.spring_6_webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Iterable<Book> findAll(){
+        return bookRepository.findAll();
+    }
+}
