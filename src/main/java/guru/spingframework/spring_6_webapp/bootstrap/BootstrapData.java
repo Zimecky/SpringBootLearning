f75045770f9c2d85f7e6aeea94fe28cc3d63b463
Zimecki <szymon.z@vp.pl>
2025-08-04 13:03:03 +0200

    Add Publisher relations & add getter and setter & extend BootstrapData

--- src/main/java/guru/spingframework/spring_6_webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/spingframework/spring_6_webapp/bootstrap/BootstrapData.java
@@ -50,18 +50,22 @@ public class BootstrapData implements CommandLineRunner {
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
 
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My publisher");
+        publisher.setAddress("123");
+        Publisher savedPublisher = publisherRepository.save(publisher);
+
+        dddSaved.setPublisher(savedPublisher);
+        noEJBSaved.setPublisher(savedPublisher);
+
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
 
         System.out.println("In Bootstrap:");
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
-
-
-        Publisher publisher = new Publisher();
-        publisher.setPublisherName("My publisher");
-        publisher.setAddress("123");
-        publisherRepository.save(publisher);
 
         System.out.println("Publisher Count: " + publisherRepository.count());
 
